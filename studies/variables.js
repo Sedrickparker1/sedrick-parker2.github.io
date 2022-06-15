837c3223988130a9847c0135c1932eac1542d447
Sedrickparker1 <sedrick.parker@nextlevelnola.org>
2022-06-15 00:57:14 +0000

    functions.js

--- studies/variables.js
+++ studies/variables.js
@@ -1,18 +1,80 @@
 /**
- * VARIABLES:
- * Declaration
+ * VARIABLES: In JavaScript, a variable stores the data value that can be changed later on.
+ *  VARIABLES must have a unique name as show below 
  * 
- * var , let, const holding things in memory greenLight (source), 
+ * EXAMPLE var myName = 'sedrick';
+ *         let value = 'value';
+ *         const VALUE = VALUE;
  * 
- * Var 
+ * var>>
+ * .Before the advent of ES6, var declarations ruled.  
  * 
+ * .var declarations are globally scoped or function/locally scoped.
  * 
+ * var is function scoped when it is declared within a function. 
+ * This means that it is available and can be accessed only within that function!!
  * 
+ *   var greeter = "hey hi";
+    
+    Here, we can clearly see that greeter is a global scoped variable.. :-)
+
+    function newFunction() {
+        var hello = "hello";
+    }
+    the hello variable is a function scoped variable..
  * 
+    can be delcared and reassigned..
+    var myName = 'sedrick'
+    var myName = 'leeroy'   ...>    This will get no error ...sickening
+
+    .If hoisted will be undefined !
+
+    .VAR can get very bad if you choose to associate multiple variables with the same names,
+    this can generate alot of bugs in your code and make you CRY..    
+    
+    :\
+    
+    LET :}
+
+    .is more preffered for variable decleration :)
+
+    .LET is block- scoped mostly , unless made global..
+
+    .LET can be reassigned but only redeclared if they are in totaly different scopes  , otherwise
+    it will be an error..
+        EXAMPLE: 
+
+        let myName = 'weddyman';
+         myName = 'sed';
+
+    .the LET keyword is not initialized.
+     So if you try to use a let variable before declaration,
+      you'll get a Reference Error.
  * 
+
+    const :|
+
+    .Variables declared with the CONST maintain constant values
+
+    EX: const IMNOTCHANGING = 'its really not changin';
  * 
- * 
- * 
+    .cannot be redeclared or reassigned
+
+    const hey = "say Hi";
+    hey = "say Hello instead";// error: Assignment to constant variable. 
+
+    .constant objects cannot be reassigned also..
+
+    EXAMPLE const person = {name : 'sed, age: 145 , job: 'graveyard resident :)',}
+    person = {
+        no... this will not work..
+    }
+
+    .but you could do this with CONST objects   EXAMPLE : person.nickName = 'leonidas';
+
+    when hoisted CONST is not initalized , same as the LET variable....
+
+
  * 
  * 
  * 
